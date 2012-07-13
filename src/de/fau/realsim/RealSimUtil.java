cf7042234d3ee78365c853e86d9af4c7fc0b5c58
Moritz 'Morty' Strübe <morty@gmx.net>
2012-07-13 17:59:58 +0200

    Fix Output

--- src/de/fau/realsim/RealSimUtil.java
+++ src/de/fau/realsim/RealSimUtil.java
@@ -2,6 +2,10 @@ package de.fau.realsim;
 
 public final class RealSimUtil {
 	public static String idToStringInt(int id){
-		return String.format("%d.%d", id%255, id / 255 );
+		return String.format("%d.%d", id%0x100, id / 0x100 );
+	}
+	
+	public static String idToStringHex(int id){
+		return String.format("%x.%x", id%0x100, id / 0x100 );
 	}
 }
